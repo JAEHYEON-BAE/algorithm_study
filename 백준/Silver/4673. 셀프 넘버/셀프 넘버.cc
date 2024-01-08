3ed261718f1ba6046b7eff27fc4f956f960fd950
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2024-01-08 15:01:41 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -1,46 +1,10 @@
-#include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <sstream>
-#include <string>
-
-using namespace std;
-
-vector<int> v;
-
-void d(int i) {
-        stringstream ss;
-        ss << i;
-        string str = ss.str();
-
-        int result = i;
-
-        for (int n = 0; n < str.length(); n++) {
-                int j = 0;
-                stringstream sss(string(1, str[n]));
-                sss >> j;
-                result += j;
-        }
-
-        if (result <= 10000) {
-                if (find(v.begin(), v.end(), result) == v.end()) {
-                        v.push_back(result);
-                }
-                d(result);
-        } else return;
-}
+#include <stdio.h>
 
 int main() {
-
-        for (int i = 1; i <= 10000; i++) {
-                if (find(v.begin(), v.end(), i) == v.end()) {
-                        d(i);
-                }
-        }
-
-        for (int i = 1; i <= 10000; i++) {
-                if (find(v.begin(), v.end(), i) == v.end()) {
-                        printf("%d\n", i);
-                }
-        }
+        int n[10035] = {1}, i=9999;
+        while (i--) 
+                n[i + i%10 + (i/10)%10 + (i/100)%10 + (i/1000)%10]++;
+        for( ; i++<10001 ;)
+                if(!n[i])
+                        printf("%d\n",i);
 }
