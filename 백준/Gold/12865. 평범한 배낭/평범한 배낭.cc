b5b35a81b66d992712d87e0130f420b5101246af
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2024-01-13 01:07:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 1384 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,32 +1,16 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include <ios>
 int main() 
 {
-        int n, k;
+        int n, k, w, v;
         scanf("%d %d", &n, &k);
-        int items[101][2];
+        int kns[100010] = {};
         for (int i=1;i<=n;i++) {
-                int w, v;
                 scanf("%d %d", &w, &v);
-                items[i][0] = w;
-                items[i][1] = v;
-        }
-
-        vector<vector<int>> kns(n+1, vector<int>(k+1, 0));
-
-        for (int i=1;i<=n;i++) {
-                for (int j=1;j<=k;j++) {
-                        kns[i][j] = kns[i-1][j];
-                        int w = items[i][0];
-                        int v = items[i][1];
-                        if (w <= j) {
-                                kns[i][j] = max(kns[i][j], kns[i-1][j-w] + v);
+                for (int j=k; j>=w; j--) {
+                        if (kns[j-w] + v > kns[j]) {
+                                kns[j] = kns[j-w] + v;
                         }
                 }
         }
-
-        printf("%d", kns[n][k]);
-        return 0;
+        printf("%d", kns[k]);
 }
