7d74d50a59e139b662a11d69cfafa97c03dd681a
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2024-02-02 10:17:54 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+int main()
+{
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);     std::cout.tie(NULL);
+        int n;  std::cin >> n;
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        while (n--) {
+                int x;  std::cin >> x;
+                if (x) pq.push(x);
+                else if (!pq.empty()) {
+                        std::cout << pq.top() << "\n";
+                        pq.pop();
+                }
+                else {
+                        std::cout << "0\n";
+                }
+        }
+        return 0;
+}
