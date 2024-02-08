d9c22f25752a5a85e5e949871b732443c41c2f3e
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2024-02-09 01:43:20 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.cc
+++ 백준/Silver/1074. Z/Z.cc
@@ -1,17 +1,8 @@
 #include <iostream>
-int power(const int a, const int b)
-{
-        if (b==0) return 1;
-        if (a==0) return 0;
-        if (a==1) return 1;
-        int tmp = power(a, b/2);
-        if (b%2==0)     return tmp*tmp;
-        else return tmp*tmp*a;
-}
 int find(const int n, const int r, const int c)
 {
         if (n==0) return 0;
-        int mid = power(2, n-1);
+        int mid = 1 << (n-1);
         if (r<mid && c<mid) return find(n-1, r, c);
         else if (r<mid && c>=mid) return mid*mid + find(n-1, r, c-mid);
         else if (r>=mid && c<mid) return mid*mid*2 + find(n-1, r-mid, c);
