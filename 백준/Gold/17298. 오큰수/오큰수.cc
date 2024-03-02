d4ccc023de2524f809908f6df514b15163d02e39
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2024-03-02 23:12:16 +0900

    [Gold IV] Title: 오큰수, Time: 292 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+int main()
+{
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);     std::cout.tie(NULL);
+        int n;  std::cin >> n;
+        std::stack<int> stk;
+        std::stack<int> index;
+        std::vector<int> v(n, 1000001);
+        int i=0;
+        while (n--) {
+                int x;  std::cin >> x;
+                int cnt = 0;
+                while (!stk.empty() && stk.top()<x) {
+                        v[index.top()] = x;
+                        ++cnt;
+                        stk.pop();
+                        index.pop();
+                }
+                index.push(i++);
+                stk.push(x);
+        }
+        for (int& i: v) {
+                if (i==1000001) std::cout << "-1 ";
+                else std::cout << i << ' ';
+        }
+        return 0;
+}
