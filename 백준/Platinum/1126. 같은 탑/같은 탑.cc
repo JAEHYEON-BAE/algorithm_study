d6e8f9f05d03ed4bf2f31311a9c0df005c8a53f2
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-01-27 11:16:14 +0900

    [Platinum III] Title: 같은 탑, Time: 52 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Platinum/1126. 같은 탑/같은 탑.cc
+++ 백준/Platinum/1126. 같은 탑/같은 탑.cc
@@ -10,26 +10,28 @@ int main() {
   std::cout.tie(nullptr);
 
   int n;  std::cin >> n;
-  std::vector<std::vector<int>> dp(n, std::vector<int>(500'001, -1));
+  std::vector<int> dp(500'001, -1), tmp(500'001, -1);
   // dp[i][gap]: i-th block, diff=gap, maximum height
   int block;  std::cin >> block;
-  dp[0][0] = 0;
-  dp[0][block] = block;
+  dp[0] = 0;
+  dp[block] = block;
 
   for (int i=1;i<n;++i) {
     std::cin >> block;
     for (int gap=0;gap<500'001;++gap) {
-      if (dp[i-1][gap]!=-1) {
-        dp[i][gap] = std::max(dp[i][gap], dp[i-1][gap]);
-        dp[i][gap+block] = std::max(dp[i][gap+block], dp[i-1][gap]+block);
-        if (gap>=block) dp[i][gap-block] = std::max(dp[i][gap-block], dp[i-1][gap]);
-        else dp[i][block-gap] = std::max(dp[i][block-gap], dp[i-1][gap]+block-gap);
+      if (dp[gap]!=-1) {
+        tmp[gap] = std::max(tmp[gap], dp[gap]);
+        tmp[gap+block] = std::max(tmp[gap+block], dp[gap]+block);
+        if (gap>=block) tmp[gap-block] = std::max(tmp[gap-block], dp[gap]);
+        else tmp[block-gap] = std::max(tmp[block-gap], dp[gap]+block-gap);
       }
     }
+    dp=tmp;
+    std::fill(tmp.begin(), tmp.end(), -1);
   }
 
   int result;
-  if ((result=dp[n-1][0])<= 0) result=-1;
+  if ((result=dp[0])<= 0) result=-1;
   std::cout << result;
   return 0; 
 }
