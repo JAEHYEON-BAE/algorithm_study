2fab780924f1b9797c6c6616a5a97b5ba2ae993c
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-03-13 19:24:38 +0900

    [Silver II] Title: 이진수 격자, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31910. 이진수 격자/이진수 격자.cc
+++ 백준/Silver/31910. 이진수 격자/이진수 격자.cc
@@ -10,7 +10,7 @@ const int INF=(~0u)>>2;
 const int nINF=(1<<31);
 const int power10[10]={1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000};
 
-long long int DFS(int node, int depth, std::vector<int> &dp, const std::bitset<900> &map, const int &n)
+long long int DFS(int node, int depth, std::vector<long long int> &dp, const std::bitset<900> &map, const int &n)
 {
   if (depth==0) return map[node];
   if (dp[node]!=-1) return dp[node];
@@ -19,8 +19,9 @@ long long int DFS(int node, int depth, std::vector<int> &dp, const std::bitset<9
   if (r<n-1) down=DFS(node+n, depth-1, dp, map, n);
   if (c<n-1) right=DFS(node+1, depth-1, dp, map, n);
 
-  return dp[node]=(map[node]<<depth)+std::max(down, right);
-
+  long long int tmp=0;
+  if (map[node]) tmp=1LL<<depth;
+  return dp[node]=(tmp)+std::max(down, right);
 }
 
 int main() {
@@ -35,7 +36,7 @@ int main() {
     map[i*n+j]=x;
   }
 
-  std::vector<int> dp(n*n, -1);
+  std::vector<long long int> dp(n*n, -1);
   std::cout << DFS(0, n+n-2, dp, map, n);
    
   return 0;
