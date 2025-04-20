ed9a191419d67dc673c35eeb72331d0006834bee
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-04-20 15:12:44 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,8 @@
+SELECT      PRODUCT_ID
+        ,   PRODUCT_NAME
+        ,   PRODUCT_CD
+        ,   CATEGORY
+        ,   PRICE
+FROM        FOOD_PRODUCT 
+ORDER BY    PRICE DESC
+FETCH       FIRST 1 ROW ONLY;
