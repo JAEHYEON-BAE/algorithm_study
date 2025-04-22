12a08c73044f22a8ad3965313ddaa7284767e2e2
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-04-22 19:38:11 +0900

    [Gold III] Title: 소수 4개의 합, Time: 552 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/14905. 소수 4개의 합/소수 4개의 합.cc
+++ 백준/Gold/14905. 소수 4개의 합/소수 4개의 합.cc
@@ -12,37 +12,6 @@ static constexpr int INF=(~0u)>>1;
 static constexpr int nINF=(1<<31);
 static constexpr long long int power10[19]={1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000, 10'000'000'000, 100'000'000'000, 1'000'000'000'000, 10'000'000'000'000, 100'000'000'000'000, 1'000'000'000'000'000, 10'000'000'000'000'000, 100'000'000'000'000'000, 1'000'000'000'000'000'000};
 
-static int FLAG=0;
-
-void DFS(int depth, int sum, const int &n, std::vector<int> &path, int tmp, const std::vector<int> &primes, const std::vector<bool> &is_prime)
-{
-  if (FLAG) return;
-  
-  if (depth==4) {
-    if (sum==n) {
-      for (const int &x:path) std::cout << x << ' ';
-      FLAG=1;
-    }
-    return;
-  }
-
-  if (depth==3 && n>sum && is_prime[n-sum]) {
-    path[3]=n-sum;
-    for (const int &x:path) std::cout << x << ' ';
-    FLAG=1;
-    return;
-  }
-
-  if (sum+(3-depth)*2>n) return;
-
-  for (int i=tmp;i<primes.size();++i) {
-    if (sum+primes[i]>n) break;
-    path[depth]=primes[i];
-    tmp=i;
-    DFS(depth+1, sum+primes[i], n, path, tmp, primes, is_prime);
-  }
-}
-
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr); 
@@ -56,12 +25,7 @@ int main() {
     }
   }
 
-  std::vector<int> primes;
-  for (int i=0;i<=100'000'000;++i) {
-    if (is_prime[i]) primes.push_back(i);
-  }
-  
-  
+
   int n;
   while (std::cin >> n) {
     if (n<8) {
@@ -69,10 +33,20 @@ int main() {
       continue;
     }
 
-    std::vector<int> path(4);
-    FLAG=0;
-    DFS(0, 0, n, path, 0, primes, is_prime);  
-    std::cout << '\n';
+    if (n&1) {
+      std::cout << "2 3 ";
+      n-=5;
+    }
+    else {
+      std::cout << "2 2 ";
+      n-=4;
+    }
+    for (int i=2;;++i) {
+      if (is_prime[i] && is_prime[n-i]) {
+        std::cout << i << ' ' << n-i << '\n';
+        break;
+      }
+    }
   }
   return 0;
 }
