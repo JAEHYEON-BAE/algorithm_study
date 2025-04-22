e743b8a8640a2b1ca94208ddcc9626b49460ccf6
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-04-22 19:42:36 +0900

    [Gold III] Title: 소수 4개의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14905. 소수 4개의 합/소수 4개의 합.cc
+++ 백준/Gold/14905. 소수 4개의 합/소수 4개의 합.cc
@@ -3,28 +3,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
-#include <queue>
 
 #define DEBUG(a) std::cerr << #a << " = " << a << std::endl
 
-static constexpr int INF=(~0u)>>1;
-static constexpr int nINF=(1<<31);
-static constexpr long long int power10[19]={1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000, 10'000'000'000, 100'000'000'000, 1'000'000'000'000, 10'000'000'000'000, 100'000'000'000'000, 1'000'000'000'000'000, 10'000'000'000'000'000, 100'000'000'000'000'000, 1'000'000'000'000'000'000};
+bool is_prime(int n) 
+{
+  for (int i=2;i*i<=n;++i) {
+    if (n%i==0) return 0;
+  }
+  return 1;
+}
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  std::vector<bool> is_prime(100'000'001, 1);
-  is_prime[0]=is_prime[1]=0;
-  for (int i=2;i*i<=100'000'000;++i) {
-    if (is_prime[i]) for (int j=i<<1;j<=100'000'000;j+=i) {
-      is_prime[j]=0;
-    }
-  }
-
+  // std::vector<bool> is_prime(100'000'001, 1);
+  // is_prime[0]=is_prime[1]=0;
+  // for (int i=2;i*i<=100'000'000;++i) {
+  //   if (is_prime[i]) for (int j=i<<1;j<=100'000'000;j+=i) {
+  //     is_prime[j]=0;
+  //   }
+  // }
 
   int n;
   while (std::cin >> n) {
@@ -42,7 +43,7 @@ int main() {
       n-=4;
     }
     for (int i=2;;++i) {
-      if (is_prime[i] && is_prime[n-i]) {
+      if (is_prime(i) && is_prime(n-i)) {
         std::cout << i << ' ' << n-i << '\n';
         break;
       }
