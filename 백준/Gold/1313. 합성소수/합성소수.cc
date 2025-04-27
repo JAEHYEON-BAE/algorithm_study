386adbfa50f3ab9e7f3198e228d5855ad7d0a39a
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-04-27 11:29:51 +0900

    [Gold IV] Title: 합성소수, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1313. 합성소수/합성소수.cc
+++ 백준/Gold/1313. 합성소수/합성소수.cc
@@ -2,15 +2,7 @@
 
 #include <iostream>
 #include <vector>
-#include <cassert>
 #include <algorithm>
-#include <cmath>
-
-#define DEBUG(a) std::cerr << #a << " = " << a << std::endl
-
-static constexpr int INF=(~0u)>>1;
-static constexpr int nINF=(1<<31);
-static constexpr long long int power10[19]={1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000, 10'000'000'000, 100'000'000'000, 1'000'000'000'000, 10'000'000'000'000, 100'000'000'000'000, 1'000'000'000'000'000, 10'000'000'000'000'000, 100'000'000'000'000'000, 1'000'000'000'000'000'000};
 
 static constexpr int _p[]={
 111,
