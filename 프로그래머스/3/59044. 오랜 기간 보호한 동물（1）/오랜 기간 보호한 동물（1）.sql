ec9ccb88191ec431945575dfde626bd8fcbbe801
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-04-27 19:46:33 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,8 @@
+SELECT i.name,
+        i.datetime
+FROM animal_ins i
+LEFT JOIN animal_outs o
+    ON i.animal_id=o.animal_id
+WHERE o.animal_id IS NULL
+ORDER BY i.datetime ASC
+FETCH FIRST 3 ROWS ONLY;
