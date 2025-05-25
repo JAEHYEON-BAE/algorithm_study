a5a94c025c1df3ffd36f06b18b51c955f09ed37c
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-05-25 15:43:58 +0900

    [Silver II] Title: 간식 파티, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/20162. 간식 파티/간식 파티.cc
+++ 백준/Silver/20162. 간식 파티/간식 파티.cc
@@ -5,7 +5,6 @@
 #include <string>
 #include <algorithm>
 #include <cassert>
-#include <map>
 
 int main() {
   std::ios_base::sync_with_stdio(false);
@@ -13,22 +12,13 @@ int main() {
   std::cout.tie(nullptr);
 
   int n;  std::cin >> n;
-  std::map<int, int> dp;
-  dp[0]=0;
-  
-  for (int i=0;i<n;++i) {
-    int k;  std::cin >> k;
-    
-    std::map<int, int> tmp;
-    for (const auto &x:dp) {
-      tmp[x.first]=std::max(tmp[x.first], x.second);
-      if (x.first<k) tmp[k]=std::max(tmp[k], x.second+k); 
-    }
-    dp=tmp;
-  }
+  std::vector<int> v(n);
+  for (int i=0;i<n;++i) std::cin >> v[i];
 
-  int max=0;
-  for (const auto &x:dp) max=std::max(max, x.second);
-  std::cout << max;
+  std::vector<int> dp=v;
+  for (int i=0;i<n;++i) for (int j=0;j<i;++j) {
+    if (v[i]>v[j]) dp[i]=std::max(dp[i], dp[j]+v[i]);
+  }
+  std::cout << *std::max_element(dp.begin(), dp.end());
   return 0;
 }
