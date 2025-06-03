b81e4d739eb43bdbb4109c05a039476e499ac028
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-06-03 13:06:35 +0900

    [Silver III] Title: 장난감 자물쇠, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/32373. 장난감 자물쇠/장난감 자물쇠.cc
+++ 백준/Silver/32373. 장난감 자물쇠/장난감 자물쇠.cc
@@ -5,34 +5,6 @@
 #include <string>
 #include <algorithm>
 #include <cassert>
-#include <array>
-
-
-#define DEBUG(a) std::cerr << #a << " = " << a << std::endl
-
-static constexpr int INF=(~0u)>>2;
-static constexpr int nINF=(1<<31);
-static constexpr long long int power10[19]={1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000, 10'000'000'000, 100'000'000'000, 1'000'000'000'000, 10'000'000'000'000, 100'000'000'000'000, 1'000'000'000'000'000, 10'000'000'000'000'000, 100'000'000'000'000'000, 1'000'000'000'000'000'000};
-
-template <typename T>
-inline void print_v(const std::vector<std::vector<T>> &v)
-{
-  for (auto x:v) {
-    for (auto y:x) std::cout << y << ' ';
-    std::cout << '\n';
-  }
-  std::cout << std::endl;
-  return;
-}
-template <typename T, size_t N>
-inline void print_v(const T (&v)[N][N])
-{
-  for (int i=0;i<N;++i) {
-    for (int j=0;j<N;++j) std::cout << v[i][j] << ' ';
-    std::cout << '\n';
-  }
-  std::cout << std::endl;
-}
 
 
 int main() {
@@ -43,7 +15,7 @@ int main() {
   int n, k;  std::cin >> n >> k;
   for (int i=0;i<n;++i) {
     int x; std::cin >> x;
-    if (std::abs(i-x)%k!=0) {
+    if ((x-i)%k!=0) {
       std::cout << "No";
       return 0;
     }
