fecaae9e796e2f981182571f8e84bdcbb3e8559a
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-06-03 12:31:14 +0900

    [Gold IV] Title: 미니 버킷 리스트, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/24270. 미니 버킷 리스트/미니 버킷 리스트.cc
+++ 백준/Gold/24270. 미니 버킷 리스트/미니 버킷 리스트.cc
@@ -8,29 +8,20 @@
 
 static constexpr long long int MOD=1'000'000'007;
 
-int solve(int n, int r) 
-{
-  long long int res=1;
-  for (int i=r+1;i<=r+n;++i) {
-    res*=i;
-    res%=MOD;
-  }
-  return res;
-}
-
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
   int n, k;  std::cin >> n >> k;
-  std::vector<int> v(n);
   int r=0;
   for (int i=0;i<n;++i) {
-    std::cin >> v[i];
-    r+=v[i];
+    int x;  std::cin >> x;
+    r+=x;
   }
   r=k-r;
-  std::cout << solve(n, r);
+  long long int res=1;
+  for (int i=r+1;i<=r+n;++i) res=res*i%MOD;
+  std::cout << res;
   return 0;
 }
