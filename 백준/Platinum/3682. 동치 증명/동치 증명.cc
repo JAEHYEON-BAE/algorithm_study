45a25ea104ccdc60f1b22ad0cd636954915faac6
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-06-15 14:21:29 +0900

    [Platinum II] Title: 동치 증명, Time: 44 ms, Memory: 7408 KB -BaekjoonHub

--- 백준/Platinum/3682. 동치 증명/동치 증명.cc
+++ 백준/Platinum/3682. 동치 증명/동치 증명.cc
@@ -7,7 +7,154 @@
 #include <unordered_set>
 #include <stack>
 
-#define DEBUG(a) std::cerr << #a << " = " << a << std::endl
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <type_traits>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE 100'000
+  #define OUTPUT_BUFFER_SIZE 1'000'000
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c<'!';c=get());
+    for (;c>='!';c=get()) s+=c;
+  }
+  void read(long double &ld) 
+  {
+    static char c; ld=0.0L;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) ld=ld*10+(c-'0');
+    if (c=='.') {
+      long double t=1.0L;
+      for (c=get();c>='0'&&c<='9';c=get()) {
+        t/=10;
+        ld+=t*(c-'0');
+      }
+    }
+    if (MINUS_FLAG) ld*=-1.0L;
+  }
+  void readline(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c!='\n'&&c!=EOF;c=get()) s+=c;
+  }
+
+  /* OUTPUT */
+  char obuf[OUTPUT_BUFFER_SIZE];
+  int opos=0;
+  inline void flush() 
+  {
+    fwrite(obuf, 1, opos, stdout);
+    opos=0;
+  }
+  inline void put(char c)
+  {
+    if (opos==OUTPUT_BUFFER_SIZE) flush();
+    obuf[opos++]=c;
+  }
+  void write(int x)
+  {
+    if (x==0) return put('0');
+    char tmp[11]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(size_t x)
+  {
+    if (x==0) return put('0');
+    char tmp[12]; int len=0;
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long long int x)
+  {
+    if (x==0) return put('0');
+    char tmp[20]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long double x, size_t precision=3)
+  {
+    if (x<0) {put('-');x*=-1.0L;}
+    if (precision==0) {
+      return write(static_cast<long long int>(std::round(x)));
+    }
+    long long int int_part=static_cast<long long int>(x);
+    write(int_part);
+    x-=int_part;
+    put('.');
+    x=std::round(x*std::pow(10, precision));
+    if (x==0) for (size_t i=0;i<precision;++i) put('0');
+    else write(static_cast<long long int>(x));
+  }
+  inline void write(const std::string &s)
+  {
+    for (const char& c:s) put(c);
+  }
+  inline void write(const char &c) 
+  {
+    put(c);
+  }
+  template <typename T>
+  inline typename std::enable_if<std::is_same<T, long double>::value>::type writeln(T x, size_t precision=3)
+  {
+    write(x, precision);
+    put('\n');
+  }
+  template <typename T>
+  inline typename std::enable_if<!std::is_same<T, long double>::value>::type writeln(T x)
+  {
+    write(x);
+    put('\n');
+  }
+  struct FASTIO_FLUSHER
+  {
+    ~FASTIO_FLUSHER() {flush();}
+  } fastio_flusher;
+}
+
+#endif
 
 int Tarjans(int node, const std::vector<std::vector<int>> &graph, std::vector<int> &d, std::vector<int> &SCC_id, int &scc_n, std::stack<int> &stk, int &id, std::vector<bool> &done)
 {
@@ -38,20 +185,20 @@ int Tarjans(int node, const std::vector<std::vector<int>> &graph, std::vector<in
 }
 
 
-
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  int t;  std::cin >> t;
+  using namespace fastio;
+  int t;  read(t);
   while (t--) {
-    int n, m;  std::cin >> n >> m;
-    if (m==0) {std::cout << n << '\n'; continue;}
+    int n, m;  read(n), read(m);
+    if (m==0) {writeln(n); continue;}
 
     std::vector<std::vector<int>> graph(n);
     for (int i=0;i<m;++i) {
-      int a, b;  std::cin >> a >> b;
+      int a, b; read(a), read(b);
       --a;--b;
       graph[a].push_back(b);
     }
@@ -65,7 +212,7 @@ int main() {
       Tarjans(i, graph, d, SCC_id, scc_n, stack, id, done);
     }
 
-    if (scc_n==1) {std::cout << "0\n"; continue;}
+    if (scc_n==1) {writeln('0'); continue;}
 
     std::vector<int> in_degree(scc_n, 0), out_degree(scc_n, 0);
     
@@ -81,7 +228,7 @@ int main() {
       if (out_degree[i]==0) ++sink;
     }
 
-    std::cout << std::max(source, sink) << '\n';
+    writeln(std::max(source, sink));
   }
  
   return 0;
