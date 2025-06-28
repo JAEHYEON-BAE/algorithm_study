1be0ae1b4d29f55056caeb4b7dcdc9919854a1c8
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-06-28 09:57:33 +0900

    [Platinum V] Title: 임계경로, Time: 56 ms, Memory: 25184 KB -BaekjoonHub

---
+++ 백준/Platinum/1948. 임계경로/임계경로.cc
@@ -0,0 +1,104 @@
+// 1948
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+#include <queue>
+
+class _1948
+{
+  const int &n;
+  const std::vector<std::unordered_map<int, int>> &graph;
+  const std::vector<std::unordered_map<int, int>> &rgraph;
+  std::vector<int> &in_degree;
+  std::vector<int> topo;
+
+public:
+  _1948(const int &n, const std::vector<std::unordered_map<int, int>> &graph, const std::vector<std::unordered_map<int, int>> &rgraph, std::vector<int> &in_degree): n(n), graph(graph), rgraph(rgraph), in_degree(in_degree) {}
+
+  void topological_ordering(std::vector<int> &dp, std::vector<int> &prev) {
+    std::queue<int> q;
+    for (int i=0;i<n;++i) if (!in_degree[i]) q.push(i);
+
+    while (!q.empty()) {
+      int curr=q.front();
+      q.pop();
+      for (const auto &[next, w]:graph[curr]) {
+        if (dp[next]<dp[curr]+w) {
+          dp[next]=dp[curr]+w;
+          prev[next]=curr;
+        }
+        if (!--in_degree[next]) q.push(next);
+      }
+    }
+  }
+
+  int count(const int &e, const std::vector<int> &dp) {
+    std::vector<std::vector<bool>> visited_e(n, std::vector<bool>(n, 0));
+    std::vector<bool> visited_v(n, 0);
+    int cnt=0;
+    
+    std::queue<int> q;
+    q.push(e);
+
+    while (!q.empty()) {
+      int curr=q.front();
+      q.pop();
+      for (const auto &[prev, w]:rgraph[curr]) {
+        if (dp[curr]==dp[prev]+w && !visited_e[prev][curr]) {
+          ++cnt;
+          visited_e[prev][curr]=1;
+
+          if (!visited_v[prev]) {
+            visited_v[prev]=1;
+            q.push(prev);
+          }
+        }
+      }
+    }
+    return cnt;
+  }
+
+  void solve(int s, int e) {
+    
+    std::vector<int> dp(n, -1);
+    std::vector<int> prev(n, -1);
+    dp[s]=0;
+
+    // Topological Ordering + Longest Path
+    topological_ordering(dp, prev);
+
+    std::cout << dp[e] << '\n' << count(e, dp);
+  }
+
+};
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n, m;  std::cin >> n >> m;
+
+  std::vector<std::unordered_map<int, int>> graph(n), rgraph(n);
+  std::vector<int> in_degree(n, 0);
+  for (int i=0;i<m;++i) {
+    int u, v, w;
+    std::cin >> u >> v >> w;
+    --u;--v;
+    graph[u][v]=w;
+    rgraph[v][u]=w;
+    ++in_degree[v];
+  }
+  int s, e;  std::cin >> s >> e;
+  --s;--e;
+
+  _1948 x(n, graph, rgraph, in_degree);
+  x.solve(s, e);
+
+  
+  return 0;
+}
