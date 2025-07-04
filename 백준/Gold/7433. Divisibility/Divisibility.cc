726c2653472ea93c672f753d92122c5a9f2b3308
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-04 17:25:52 +0900

    [Gold IV] Title: Divisibility, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7433. Divisibility/Divisibility.cc
+++ 백준/Gold/7433. Divisibility/Divisibility.cc
@@ -12,24 +12,18 @@ int main()
   std::cout.tie(nullptr);
 
   int n, k;  std::cin >> n >> k;
-  // std::vector<bool> mod(k, 0);
-
-  auto mod=[&k](int x) {
-    return ((x%k)+k)%k;
-  };
-  
-  std::vector<std::bitset<100>> dp(n, 0);
+  std::bitset<100> mod(0);
   int x;  std::cin >> x;
-  dp[0][mod(x)]=1;
+  x=(x%k+k)%k;
+  mod[x]=1;
   
   for (int i=1;i<n;++i) {
     std::cin >> x;
-    for (int j=0;j<k;++j) {
-      if (dp[i-1][j]) dp[i][mod(j+x)]=dp[i][mod(j-x)]=1;
-    }
+    x=(x%k+k)%k;
+    mod=(mod<<x) | (mod>>(k-x)) | (mod>>x) | (mod<<(k-x));
   }
 
-  std::cout << ((dp[n-1][0])?"Divisible":"Not divisible");
+  std::cout << (mod[0]?"Divisible":"Not divisible");
 
   return 0;
 }
