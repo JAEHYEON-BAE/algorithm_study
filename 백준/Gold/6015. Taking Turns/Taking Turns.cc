a9b7be475ee7351adecc139147f1b95045536867
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-04 20:11:39 +0900

    [Gold II] Title: Taking Turns, Time: 16 ms, Memory: 8516 KB -BaekjoonHub

--- 백준/Gold/6015. Taking Turns/Taking Turns.cc
+++ 백준/Gold/6015. Taking Turns/Taking Turns.cc
@@ -4,15 +4,59 @@
 #include <vector>
 #include <algorithm>
 
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+#include <string>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+}
+
+#endif
+
 int main() 
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  int n;  std::cin >> n;
+  using namespace fastio;
+  
+  int n;  read(n);
   std::vector<long long int> w(n);
-  for (int i=0;i<n;++i) std::cin >> w[i];
+  for (int i=0;i<n;++i) read(w[i]);
 
   long long int f=0, s=0;
   for (int i=n-1;i>=0;--i) {
