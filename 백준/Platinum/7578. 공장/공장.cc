f963d2e7dd6ad9746cafe75afd9584371952f4a6
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-06 11:15:27 +0900

    [Platinum V] Title: 공장, Time: 324 ms, Memory: 28436 KB -BaekjoonHub

---
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -0,0 +1,110 @@
+// 7578
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+}
+
+#endif
+
+class FenwickTree 
+{
+  int size;
+  std::vector<long long int> tree;
+
+  long long int find(int idx) const {
+    long long int result=0;
+    while (idx>0) {
+      result+=tree[idx];
+      idx-=(idx&-idx);
+    }
+    return result;
+  }
+
+public:
+  FenwickTree(const int &n) {
+    size=1;
+    while (size<n) size<<=1;
+    tree.assign(size+1, 0);
+  }
+
+  void insert(int idx) {
+    while (idx<=size) {
+      ++tree[idx];
+      idx+=(idx&-idx);
+    }
+  }
+
+  long long int query(int x) const {
+    return find(size)-find(x);
+  }
+};
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  using namespace fastio;
+
+  int n;  read(n);
+
+  std::unordered_map<int, int> map;
+  for (int i=0;i<n;++i) {
+    int x;  read(x);
+    map[x]=i+1;
+  }
+
+  FenwickTree ft(n+1);
+
+  long long int cnt=0;
+  for (int i=0;i<n;++i) {
+    int x;  read(x);
+    x=map.at(x);
+    cnt+=ft.query(x);
+    ft.insert(x);
+  }
+  std::cout << cnt;
+  
+  return 0;
+}
