49f3989fe8e54da03b07ab485d3055ab8dcc3b81
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-06 16:15:08 +0900

    [Platinum V] Title: 개미, Time: 120 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Platinum/14942. 개미/개미.cc
@@ -0,0 +1,232 @@
+// 14942
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <type_traits>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  #define OUTPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c<'!';c=get());
+    for (;c>='!';c=get()) s+=c;
+  }
+  void read(long double &ld) 
+  {
+    static char c; ld=0.0L;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) ld=ld*10+(c-'0');
+    if (c=='.') {
+      long double t=1.0L;
+      for (c=get();c>='0'&&c<='9';c=get()) {
+        t/=10;
+        ld+=t*(c-'0');
+      }
+    }
+    if (MINUS_FLAG) ld*=-1.0L;
+  }
+  void readline(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c!='\n'&&c!=EOF;c=get()) s+=c;
+  }
+
+  /* OUTPUT */
+  char obuf[OUTPUT_BUFFER_SIZE];
+  int opos=0;
+  inline void flush() 
+  {
+    fwrite(obuf, 1, opos, stdout);
+    opos=0;
+  }
+  inline void put(char c)
+  {
+    if (opos==OUTPUT_BUFFER_SIZE) flush();
+    obuf[opos++]=c;
+  }
+  void write(int x)
+  {
+    if (x==0) return put('0');
+    char tmp[11]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(size_t x)
+  {
+    if (x==0) return put('0');
+    char tmp[12]; int len=0;
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long long int x)
+  {
+    if (x==0) return put('0');
+    char tmp[20]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long double x, size_t precision=3)
+  {
+    if (x<0) {put('-');x*=-1.0L;}
+    if (precision==0) {
+      return write(static_cast<long long int>(std::round(x)));
+    }
+    long long int int_part=static_cast<long long int>(x);
+    write(int_part);
+    x-=int_part;
+    put('.');
+    x=std::round(x*std::pow(10, precision));
+    if (x==0) for (size_t i=0;i<precision;++i) put('0');
+    else write(static_cast<long long int>(x));
+  }
+  inline void write(const std::string &s)
+  {
+    for (const char& c:s) put(c);
+  }
+  inline void write(const char *s)
+  {
+    while (*s) put(*s++);
+  }
+  inline void write(const char &c) 
+  {
+    put(c);
+  }
+  inline void write(const bool &b)
+  {
+    if (b) put('1');
+    else put('0');
+  }
+  template <typename T>
+  inline typename std::enable_if<std::is_same<T, long double>::value>::type writeln(T x, size_t precision=3)
+  {
+    write(x, precision);
+    put('\n');
+  }
+  template <typename T>
+  inline typename std::enable_if<!std::is_same<T, long double>::value>::type writeln(T x)
+  {
+    write(x);
+    put('\n');
+  }
+  struct FASTIO_FLUSHER
+  {
+    ~FASTIO_FLUSHER() {flush();}
+  } fastio_flusher;
+}
+
+#endif
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  using namespace fastio;
+
+  int n;  read(n);
+  std::vector<int> e(n);
+  for (int i=0;i<n;++i) read(e[i]);
+
+  std::vector<std::unordered_map<int, int>> graph(n);
+  for (int i=0;i<n-1;++i) {
+    int a, b, c;  read(a), read(b), read(c);
+    --a;--b;
+    graph[a][b]=c;
+    graph[b][a]=c;
+  }
+
+  std::vector<int> d(n, 0);
+
+  std::queue<int> q;
+  q.push(0);
+  d[0]=0;
+  std::vector<int> parent(n, -1);
+  while (!q.empty()) {
+    int curr=q.front();
+    q.pop();
+    for (const auto &[next, dist]:graph[curr]) {
+      graph[next].erase(curr);
+      parent[next]=curr;
+      d[next]=d[curr]+dist;
+      q.push(next);
+    }
+  }
+
+  std::vector<std::vector<int>> sparse_table(n, std::vector<int>(18, -1));
+  for (int i=1;i<n;++i) {
+    sparse_table[i][0]=parent[i];
+  }
+  for (int k=1;k<18;++k) for (int i=0;i<n;++i) {
+    if (sparse_table[i][k-1]!=-1) {
+      int mid=sparse_table[i][k-1];
+      sparse_table[i][k]=sparse_table[mid][k-1];
+    }
+  }
+
+  for (int i=0;i<n;++i) {
+    if (e[i]>=d[i]) writeln('1');
+    else {
+      int energy=e[i];
+      int curr=i;
+      for (int k=17;k>=0;--k) {
+        int j=sparse_table[curr][k];
+        if (j!=-1 && d[curr]-d[j]<=energy) {
+          energy-=d[curr]-d[j];
+          curr=j;
+        } 
+      }
+      writeln(curr+1);
+    }
+  }
+  
+  return 0;
+}
