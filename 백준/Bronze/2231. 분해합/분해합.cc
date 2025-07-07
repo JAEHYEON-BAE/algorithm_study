446252f3b5fc6999b8f831cd89f8c726ef6d3c4f
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-07 12:21:46 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,29 @@
+// 2231
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n;  std::cin >> n;
+
+  for (int i=0;i<n;++i) {
+    int tmp=i;
+    int sum=tmp;
+    while (tmp) {
+      sum+=tmp%10;
+      tmp/=10;
+    }
+    if (sum==n) {
+      std::cout << i;
+      return 0;
+    }
+  }
+  std::cout << '0';
+  return 0;
+}
