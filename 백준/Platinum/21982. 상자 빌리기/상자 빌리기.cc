0aa46e47668a7a6d85d4dedc0f0c526af1f62d00
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-09 15:46:08 +0900

    [Platinum V] Title: 상자 빌리기, Time: 2084 ms, Memory: 121452 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Platinum/21982. 상자 빌리기/상자 빌리기.cc
@@ -0,0 +1,210 @@
+// 21982
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <type_traits>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  #define OUTPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c<'!';c=get());
+    for (;c>='!';c=get()) s+=c;
+  }
+  void read(long double &ld) 
+  {
+    static char c; ld=0.0L;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) ld=ld*10+(c-'0');
+    if (c=='.') {
+      long double t=1.0L;
+      for (c=get();c>='0'&&c<='9';c=get()) {
+        t/=10;
+        ld+=t*(c-'0');
+      }
+    }
+    if (MINUS_FLAG) ld*=-1.0L;
+  }
+  void readline(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c!='\n'&&c!=EOF;c=get()) s+=c;
+  }
+
+  /* OUTPUT */
+  char obuf[OUTPUT_BUFFER_SIZE];
+  int opos=0;
+  inline void flush() 
+  {
+    fwrite(obuf, 1, opos, stdout);
+    opos=0;
+  }
+  inline void put(char c)
+  {
+    if (opos==OUTPUT_BUFFER_SIZE) flush();
+    obuf[opos++]=c;
+  }
+  void write(int x)
+  {
+    if (x==0) return put('0');
+    char tmp[11]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(size_t x)
+  {
+    if (x==0) return put('0');
+    char tmp[12]; int len=0;
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long long int x)
+  {
+    if (x==0) return put('0');
+    char tmp[20]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long double x, size_t precision=3)
+  {
+    if (x<0) {put('-');x*=-1.0L;}
+    if (precision==0) {
+      return write(static_cast<long long int>(std::round(x)));
+    }
+    long long int int_part=static_cast<long long int>(x);
+    write(int_part);
+    x-=int_part;
+    put('.');
+    x=std::round(x*std::pow(10, precision));
+    if (x==0) for (size_t i=0;i<precision;++i) put('0');
+    else write(static_cast<long long int>(x));
+  }
+  inline void write(const std::string &s)
+  {
+    for (const char& c:s) put(c);
+  }
+  inline void write(const char *s)
+  {
+    while (*s) put(*s++);
+  }
+  inline void write(const char &c) 
+  {
+    put(c);
+  }
+  inline void write(const bool &b)
+  {
+    if (b) put('1');
+    else put('0');
+  }
+  template <typename T>
+  inline typename std::enable_if<std::is_same<T, long double>::value>::type writeln(T x, size_t precision=3)
+  {
+    write(x, precision);
+    put('\n');
+  }
+  template <typename T>
+  inline typename std::enable_if<!std::is_same<T, long double>::value>::type writeln(T x)
+  {
+    write(x);
+    put('\n');
+  }
+  struct FASTIO_FLUSHER
+  {
+    ~FASTIO_FLUSHER() {flush();}
+  } fastio_flusher;
+}
+
+#endif
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  using namespace fastio;
+  int t, n, x;
+  read(t);
+  while (t--) {
+    read(n), read(x);
+    long long int hs, ha, hb, hc;
+    read(hs), read(ha), read(hb), read(hc);
+    long long int ws, wa, wb, wc;
+    read(ws), read(wa), read(wb), read(wc);
+
+    std::vector<long long int> h(n), w(n), v(n);
+    h[0]=hs%hc+1;
+    w[0]=ws%wc+1;
+    v[0]=h[0]*w[0];
+    for (int i=1;i<n;++i) {
+      h[i]=h[i-1]+1+(h[i-1]*ha+hb)%hc;
+      w[i]=(w[i-1]*wa+wb)%wc+1;
+      
+      v[i]=h[i]*w[i];
+    }
+
+    long long int max=-1;
+    
+    std::deque<int> dq;
+    dq.push_back(0);
+    
+    for (int i=1;i<n;++i) {
+      while (!dq.empty() && h[dq.front()]<h[i]-x) dq.pop_front();
+      if (!dq.empty()) max=std::max(max, v[dq.front()]+v[i]);
+
+      while (!dq.empty() && v[dq.back()]<v[i]) dq.pop_back();
+      dq.push_back(i);
+    }
+
+    writeln(max);
+  }
+  
+  return 0;
+}
