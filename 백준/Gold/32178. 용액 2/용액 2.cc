b9e448d80490fbdbd81400e3571d72762592c6a0
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-10 12:53:40 +0900

    [Gold II] Title: 용액 2, Time: 140 ms, Memory: 18676 KB -BaekjoonHub

--- 백준/Gold/32178. 용액 2/용액 2.cc
+++ 백준/Gold/32178. 용액 2/용액 2.cc
@@ -3,8 +3,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+}
+
+#endif
 
 int main() 
 {
@@ -12,18 +51,14 @@ int main()
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  // using namespace fastio;
+  using namespace fastio;
 
-  int n;  
-  // read(n);
-  std::cin >> n;
+  int n;  read(n);
   std::vector<std::pair<long long int, int>> sum(n+1);
   sum[0]={0LL, 0};
   
   for (int i=0;i<n;++i) {
-    long long int x; 
-    // read(x);
-    std::cin >> x;
+    long long int x; read(x);
     sum[i+1]={sum[i].first+x, i+1};
   }
   std::sort(sum.begin(), sum.end());
