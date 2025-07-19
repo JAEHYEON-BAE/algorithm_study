7cd2f4a241206a3792b7ecceb56558120eecec86
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-07-19 17:24:28 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 96 ms, Memory: 5564 KB -BaekjoonHub

--- 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cc
+++ 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cc
@@ -4,8 +4,164 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <unordered_map>
 
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <type_traits>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  #define OUTPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c<'!';c=get());
+    for (;c>='!';c=get()) s+=c;
+  }
+  void read(long double &ld) 
+  {
+    static char c; ld=0.0L;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) ld=ld*10+(c-'0');
+    if (c=='.') {
+      long double t=1.0L;
+      for (c=get();c>='0'&&c<='9';c=get()) {
+        t/=10;
+        ld+=t*(c-'0');
+      }
+    }
+    if (MINUS_FLAG) ld*=-1.0L;
+  }
+  void readline(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c!='\n'&&c!=EOF;c=get()) s+=c;
+  }
+
+  /* OUTPUT */
+  char obuf[OUTPUT_BUFFER_SIZE];
+  int opos=0;
+  inline void flush() 
+  {
+    fwrite(obuf, 1, opos, stdout);
+    opos=0;
+  }
+  inline void put(char c)
+  {
+    if (opos==OUTPUT_BUFFER_SIZE) flush();
+    obuf[opos++]=c;
+  }
+  void write(int x)
+  {
+    if (x==0) return put('0');
+    char tmp[11]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(size_t x)
+  {
+    if (x==0) return put('0');
+    char tmp[12]; int len=0;
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long long int x)
+  {
+    if (x==0) return put('0');
+    char tmp[20]; int len=0;
+    if (x<0) {put('-');x*=-1;}
+    while (x) {tmp[len++]='0'+(x%10); x/=10;}
+    while (len--) put(tmp[len]);
+  }
+  void write(long double x, size_t precision=3)
+  {
+    if (x<0) {put('-');x*=-1.0L;}
+    if (precision==0) {
+      return write(static_cast<long long int>(std::round(x)));
+    }
+    long long int int_part=static_cast<long long int>(x);
+    write(int_part);
+    x-=int_part;
+    put('.');
+    x=std::round(x*std::pow(10, precision));
+    if (x==0) for (size_t i=0;i<precision;++i) put('0');
+    else write(static_cast<long long int>(x));
+  }
+  inline void write(const std::string &s)
+  {
+    for (const char& c:s) put(c);
+  }
+  inline void write(const char *s)
+  {
+    while (*s) put(*s++);
+  }
+  inline void write(const char &c) 
+  {
+    put(c);
+  }
+  inline void write(const bool &b)
+  {
+    if (b) put('1');
+    else put('0');
+  }
+  template <typename T>
+  inline typename std::enable_if<std::is_same<T, long double>::value>::type writeln(T x, size_t precision=3)
+  {
+    write(x, precision);
+    put('\n');
+  }
+  template <typename T>
+  inline typename std::enable_if<!std::is_same<T, long double>::value>::type writeln(T x)
+  {
+    write(x);
+    put('\n');
+  }
+  struct FASTIO_FLUSHER
+  {
+    ~FASTIO_FLUSHER() {flush();}
+  } fastio_flusher;
+}
+
+#endif
 
 class _3830
 {
@@ -46,10 +202,8 @@ public:
     unite(a, b, w);
   }
   void query(int a, int b) {
-    if (find(a)!=find(b)) std::cout << "UNKNOWN\n";
-    else {
-      std::cout << weight[b]-weight[a] << '\n';
-    }
+    if (find(a)!=find(b)) fastio::write("UNKNOWN\n");
+    else fastio::writeln(weight[b]-weight[a]);
   }
 };
 
@@ -59,20 +213,24 @@ int main()
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
+  using namespace fastio;
+  
   int n, m;
-  while (std::cin >> n >> m) {
+  while (1) {
+    read(n), read(m);
     if (!n && !m) break;
 
     _3830 x(n);
     for (int i=0;i<m;++i) {
-      char c; std::cin >> c;
-      if (c=='!'){
-        int a, b, w;  std::cin >> a >> b >> w;
+      std::string c; read(c);
+      int a, b, w;
+      if (c=="!"){
+        read(a), read(b), read(w);
         --a;--b;
         x.measure(a, b, w);
       }
-      else if (c=='?') {
-        int a, b;  std::cin >> a >> b;
+      else if (c=="?") {
+        read(a), read(b);
         --a;--b;
         x.query(a, b);
       }
