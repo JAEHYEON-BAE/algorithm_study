9ac1551d4d3b9bb0d7993d5819b5bfaa052fa16f
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-08-09 15:43:29 +0900

    [Platinum V] Title: Lemonade Trade, Time: 44 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Platinum/15014. Lemonade Trade/Lemonade Trade.cc
@@ -0,0 +1,111 @@
+// 15014
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <unordered_map>
+#include <iomanip>
+
+#ifndef FASTIO_HPP
+#define FASTIO_HPP
+
+#include <cstdio>
+#include <string>
+#include <cmath>
+
+namespace fastio
+{
+  #define INPUT_BUFFER_SIZE (1<<20)
+  #define OUTPUT_BUFFER_SIZE (1<<20)
+  
+  /* INPUT */
+  char get()
+  {
+    static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+    if (S==T) {
+      T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+      if (S==T) return EOF;
+    }
+    return *S++;
+  }
+  void read(int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(long long int &x)
+  {
+    static char c; x=0;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+    if (MINUS_FLAG) x*=-1;
+  }
+  void read(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c<'!';c=get());
+    for (;c>='!';c=get()) s+=c;
+  }
+  void read(long double &ld) 
+  {
+    static char c; ld=0.0L;
+    bool MINUS_FLAG=0;
+    for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+    if (c=='-') {MINUS_FLAG=1;c=get();}
+    for (;c>='0'&&c<='9';c=get()) ld=ld*10+(c-'0');
+    if (c=='.') {
+      long double t=1.0L;
+      for (c=get();c>='0'&&c<='9';c=get()) {
+        t/=10;
+        ld+=t*(c-'0');
+      }
+    }
+    if (MINUS_FLAG) ld*=-1.0L;
+  }
+  void readline(std::string &s, int size=0)
+  {
+    static char c; s="";
+    if (size) s.reserve(size);
+    for (c=get();c!='\n'&&c!=EOF;c=get()) s+=c;
+  }
+}
+
+#endif
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  using namespace fastio;
+  
+  int n;  read(n);
+
+  std::unordered_map<std::string, long double> dp;
+  dp.reserve(n<<1);
+  dp["pink"]=0.0L;
+  
+  for (int i=0;i<n;++i) {
+    std::string o, w;
+    long double r;
+    read(o), read(w), read(r);
+    if (!dp.count(w)) continue;
+    if (!dp.count(o)) dp[o]=dp[w]+std::log2(r);
+    else dp[o]=std::max(dp[o], dp[w]+std::log2(r));
+  }
+
+  std::cout << std::fixed << std::setprecision(10);
+  if (!dp.count("blue")) std::cout << 0.0;
+  else std::cout << std::min(10.0L, std::pow(2.0L, dp["blue"]));
+
+  return 0;
+}
