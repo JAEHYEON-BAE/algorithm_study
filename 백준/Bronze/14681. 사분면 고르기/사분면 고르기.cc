0372611d4343eac11011aa7eab37d60d6c3ee1e9
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-08-13 12:17:15 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int x, y;  std::cin >> x >> y;
+  if (x>0) {
+    if (y>0) std::cout << '1';
+    else std::cout << '4';
+  }
+  else {
+    if (y>0) std::cout << '2';
+    else std::cout << '3';
+  }
+  return 0;
+}
