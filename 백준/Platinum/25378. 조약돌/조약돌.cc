0c4572206fb39d7cf85182e8e8b91dfd61f2cecc
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-08-22 12:36:33 +0900

    [Platinum V] Title: 조약돌, Time: 16 ms, Memory: 3084 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25378. 조약돌/조약돌.cc
+++ 백준/Platinum/25378. 조약돌/조약돌.cc
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <array>
 
 int main() 
 {
@@ -16,19 +17,35 @@ int main()
   std::vector<int> v(n);
   for (int &i:v) std::cin >> i;
 
-  int cnt=0;
-  for (int i=0;i<n-1;++i) if (v[i]) {
-    if (v[i]<=v[i+1]) {
-      v[i+1]-=v[i];
-      ++cnt;
-    }
-    else {
-      v[i+1]=0;
-      cnt+=2;
+  std::vector<std::vector<bool>> one(n, std::vector<bool>(n, 0));
+
+  for (int l=0;l<n-1;++l) {
+    bool FLAG=1;
+    int prev=v[l];
+    for (int r=l;r<n;++r) {
+      if (prev==v[r]) one[l][r]=FLAG;
+      else {
+        one[l][r]=0;
+        if (prev>v[r]) FLAG=0;
+        prev=std::abs(prev-v[r]);
+      }
+      // std::cout << l << ' ' << r << ' ' << one[l][r] << '\n';
     }
   }
-  if (v.back()) ++cnt;
-  std::cout << cnt;
+
+  std::vector<int> dp(n, 0);
+  dp[0]=1;
+  for (int r=1;r<n;++r) {
+    dp[r]=dp[r-1]+1;
+    for (int l=0;l<r;++l) {
+      if (one[l][r]) {
+        dp[r]=std::min(dp[r], (l?dp[l-1]:0)+r-l);
+      }
+    }
+    // std::cout << "dp[" << r << "] = " << dp[r] << '\n';
+  }
+  
+  std::cout << dp[n-1];
   return 0;
 }
 
