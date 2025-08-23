f156155efe5c00c0860ba23253d81fbb873f7a55
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-08-23 15:39:31 +0900

    [Gold II] Title: 버블 소트, Time: 316 ms, Memory: 49420 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -0,0 +1,65 @@
+// 1377
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <unordered_map>
+
+#define INPUT_BUFFER_SIZE (1<<20)
+
+/* INPUT */
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n;  read(n);
+  std::vector<int> v(n);
+  for (int &i:v) read(i);
+
+  auto cpy=v;
+  std::sort(cpy.begin(), cpy.end());
+  std::unordered_map<int, int> map;
+  int id=0;
+  for (const int &i:cpy) {
+    if (map.count(i)) ++id;
+    else map[i]=id++;
+  }
+
+  std::unordered_map<int, int> cnt;
+  for (int &i:v) {
+    ++cnt[i];
+    if (cnt[i]==1) i=map[i];
+    else i=map[i]+cnt[i]-1;
+  }
+
+  int res=0;
+  for (int i=0;i<n;++i) {
+    res=std::max(res, i-v[i]);
+  }
+  std::cout << res+1;
+  return 0;
+}
+
