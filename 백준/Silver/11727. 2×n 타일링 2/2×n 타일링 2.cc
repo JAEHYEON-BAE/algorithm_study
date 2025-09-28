2439199645a4bac496b7a9997711ef75304c12cf
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-09-28 20:13:42 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,25 @@
+// 11726
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+constexpr int MOD=10'007;
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n;  std::cin >> n;
+  std::vector<int> dp(n+1, 0);
+  dp[0]=1;
+  dp[1]=1;
+  for (int i=2;i<=n;++i) {
+    dp[i]=dp[i-2]*2+dp[i-1];
+    dp[i]%=MOD;
+  }
+  std::cout << dp[n];
+  return 0;
+}
