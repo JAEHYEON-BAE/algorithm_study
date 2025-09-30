9709408665b04c304a5b4473ddb6fc1900310592
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-09-30 12:35:14 +0900

    [Platinum IV] Title: Binaria, Time: 84 ms, Memory: 9964 KB -BaekjoonHub

---
+++ 백준/Platinum/28317. Binaria/Binaria.cc
@@ -0,0 +1,93 @@
+// 28317
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <unordered_set>
+
+#define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
+
+using ll=long long int;
+constexpr ll MOD=1'000'003;
+std::vector<ll> fact;
+void init(int k)
+{
+  fact.resize(k+1);
+  fact[0]=1LL;
+  for (ll i=1;i<=k;++i) {
+    fact[i]=fact[i-1]*i%MOD;
+  }
+}
+
+ll mod_exp(ll a, int r) 
+{
+  ll tmp=1;
+  while (r) {
+    if (r&1) tmp=tmp*a%MOD;
+    a=a*a%MOD;
+    r>>=1;
+  }
+  return tmp;
+}
+
+inline ll mod_inv(int x) 
+{
+  return mod_exp(x, MOD-2);
+}
+
+ll combination(ll n, ll r) 
+{
+  ll a=fact[n];
+  ll b=mod_inv(fact[n-r]*fact[r]%MOD);
+  return a*b%MOD;
+}
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n, k;  std::cin >> n >> k;
+  init(k);
+
+  int m=n-k+1;
+  std::vector<int> a(m);
+  // d[i]:= a[i+1]-a[i],  
+  // d[i] = S[i+k]-S[i],  
+  // S[i+k] = S[i]+d[i]
+  std::vector<int> d(m-1);
+  for (int i=0;i<m;++i) {
+    std::cin >> a[i];
+    if (a[i]>k) {std::cout << '0'; return 0;}
+    if (i) {
+      d[i-1]=a[i]-a[i-1];
+      if (d[i-1]<-1 || d[i-1]>1) {std::cout << '0'; return 0;}
+    }
+  }
+
+  ll free=0;
+  ll one=0;
+  
+  for (int i=0;i<k;++i) {
+    int cur=0, min=0, max=0;
+    for (int j=i;j<m-1;j+=k) {
+      cur+=d[j];
+      min=std::min(min, cur);
+      max=std::max(max, cur);
+      if (min<-1 || max>1) {std::cout << '0'; return 0;}
+    }
+    bool _0=min==0;
+    bool _1=max==0;
+
+    if (_0 && _1) ++free;
+    else if (_1) ++one;
+    else if (!_0) {std::cout << '0'; return 0;}
+  }
+
+  if (a[0]<one || free+one<a[0]) {std::cout << '0'; return 0;}
+  // answer = Combination(free, a[0]-one)
+  std::cout << combination(free, a[0]-one);
+  return 0;
+}
