0349e2eaa5a25805682846db03d8726c6badfb13
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-10-04 13:39:49 +0900

    [Platinum IV] Title: 本棚 (Bookshelf), Time: 12 ms, Memory: 5004 KB -BaekjoonHub

--- 백준/Platinum/24107. 本棚 （Bookshelf）/本棚 （Bookshelf）.cc
+++ 백준/Platinum/24107. 本棚 （Bookshelf）/本棚 （Bookshelf）.cc
@@ -9,6 +9,37 @@
 
 using ll=long long int;
 
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+void read(ll &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+
 struct Fenwick
 {
   int n;
@@ -38,16 +69,14 @@ int main()
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  int n;  std::cin >> n;
+  int n;  read(n);
   std::vector<ll> w(n+1);
   ll tot=0;
   for (int i=1;i<=n;++i) {
-    std::cin >> w[i]; tot+=w[i];
+    read(w[i]); tot+=w[i];
   }
   std::vector<int> a(n);
-  for (int &x:a) {
-    std::cin >> x;
-  }
+  for (int &x:a) read(x);
 
   Fenwick fw(n);
 
