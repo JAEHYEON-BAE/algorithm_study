2e06da0e4a281d2c59c7868ffaec84e32f500fef
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-10-13 18:47:19 +0900

    [Platinum III] Title: 다각형 게임, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/13034. 다각형 게임/다각형 게임.cc
@@ -0,0 +1,32 @@
+// 13034
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <unordered_set>
+
+#define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n;  std::cin >> n;
+  std::vector<int> G(n+1);
+  G[0]=0;
+  G[1]=0;
+  G[2]=1;
+  for (int i=3;i<=n;++i) {
+    std::unordered_set<int> s;
+    s.insert(G[i-2]);
+    for (int j=1;j<=i-j-2;++j) s.insert(G[j]^G[i-j-2]);
+    int tmp=0;
+    while (s.count(tmp)) ++tmp;
+    G[i]=tmp;
+  }
+  std::cout << (G[n]?1:2);
+  return 0;
+}
