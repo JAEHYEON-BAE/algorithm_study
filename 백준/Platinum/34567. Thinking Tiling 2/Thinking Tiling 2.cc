30828b4cdc845193d25ce5a23a250df092a38b59
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-10-19 09:56:27 +0900

    [Platinum IV] Title: Thinking Tiling 2, Time: 8 ms, Memory: 3048 KB -BaekjoonHub

--- 백준/Platinum/34567. Thinking Tiling 2/Thinking Tiling 2.cc
+++ 백준/Platinum/34567. Thinking Tiling 2/Thinking Tiling 2.cc
@@ -7,6 +7,33 @@
 
 #define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
 
+/* OUTPUT */
+#define OUTPUT_BUFFER_SIZE (1<<20)
+
+char obuf[OUTPUT_BUFFER_SIZE];
+int opos=0;
+inline void flush() 
+{
+  fwrite(obuf, 1, opos, stdout);
+  opos=0;
+}
+inline void put(char c)
+{
+  if (opos==OUTPUT_BUFFER_SIZE) flush();
+  obuf[opos++]=c;
+}
+void write(int x)
+{
+  if (x==0) return put('0');
+  char tmp[11]; int len=0;
+  if (x<0) {put('-');x*=-1;}
+  while (x) {tmp[len++]='0'+(x%10); x/=10;}
+  while (len--) put(tmp[len]);
+}
+inline void write(const char *s)
+{
+  while (*s) put(*s++);
+}
 
 int main() 
 {
@@ -19,63 +46,81 @@ int main()
     int r, c;  std::cin >> r >> c;
 
     if (r==1 || c==1) {
-      std::cout << "0\n";
+      write("0\n");
       continue;
     }
 
     // I. 2x2만 이용
     if (!(r&1) && !(c&1)) {
-      std::cout << (r>>1)*(c>>1) << '\n';
+      write((r>>1)*(c>>1)), put('\n');
+      // std::cout << (r>>1)*(c>>1) << '\n';
       for (int i=0;i<r;i+=2) for (int j=0;j<c;j+=2) {
-        std::cout << "1 " << i+1 << ' ' << j+1 << '\n';
+        write("1 " ), write(i+1), put(' '), write(j+1), put('\n');
+        // std::cout << "1 " << i+1 << ' ' << j+1 << '\n';
       }
       continue;
     }
 
     // II. 3x3만 이용
     if (r%3==0 && c%3==0) {
-      std::cout << (r/3)*(c/3) << '\n';
+      write((r/3)*(c/3)), put('\n');
+      // std::cout << (r/3)*(c/3) << '\n';
       for (int i=0;i<r;i+=3) for (int j=0;j<c;j+=3) {
-        std::cout << "2 " << i+1 << ' ' << j+1 << '\n';
+        write("2 "), write(i+1), put(' '), write(j+1), put('\n');
+        // std::cout << "2 " << i+1 << ' ' << j+1 << '\n';
       }
       continue;
     }
 
     // III. 섞어서 이용
     if (r%6 && c%6) {
-      std::cout << "0\n";
+      write("0\n");
+      // std::cout << "0\n";
       continue;
     }
     else if (r%6==0) {
       int p=c/6, q=c%6;
       int cnt=9*p+(q==1?-1:5);
-      std::cout << cnt*(r/6) << '\n';
+      write(cnt*(r/6)), put('\n');
+      // std::cout << cnt*(r/6) << '\n';
       for (int i=0;i<r;i+=6) {
         for (int x=0;x<c-3;x+=2) {
-          std::cout << "1 " << i+1 << ' ' << x+1 << '\n';
-          std::cout << "1 " << i+3 << ' ' << x+1 << '\n';
-          std::cout << "1 " << i+5 << ' ' << x+1 << '\n';
+          write("1 "), write(i+1), put(' '), write(x+1), put('\n');
+          write("1 "), write(i+3), put(' '), write(x+1), put('\n');
+          write("1 "), write(i+5), put(' '), write(x+1), put('\n');
+          // std::cout << "1 " << i+1 << ' ' << x+1 << '\n';
+          // std::cout << "1 " << i+3 << ' ' << x+1 << '\n';
+          // std::cout << "1 " << i+5 << ' ' << x+1 << '\n';
         }
-        std::cout << "2 " << i+1 << ' ' << c-2 << '\n';
-        std::cout << "2 " << i+4 << ' ' << c-2 << '\n';
+        write("2 "), write(i+1), put(' '), write(c-2), put('\n');
+        write("2 "), write(i+4), put(' '), write(c-2), put('\n');
+        // std::cout << "2 " << i+1 << ' ' << c-2 << '\n';
+        // std::cout << "2 " << i+4 << ' ' << c-2 << '\n';
       }
       continue;
     }
     else if (c%6==0) {
       int p=r/6, q=r%6;
       int cnt=9*p+(q==1?-1:5);
-      std::cout << cnt*(c/6) << '\n';
+      write(cnt*(c/6)), put('\n');
+      // std::cout << cnt*(c/6) << '\n';
       for (int i=0;i<c;i+=6) {
         for (int x=0;x<r-3;x+=2) {
-          std::cout << "1 " << x+1 << ' ' << i+1 << '\n';
-          std::cout << "1 " << x+1 << ' ' << i+3 << '\n';
-          std::cout << "1 " << x+1 << ' ' << i+5 << '\n';
+          write("1 "), write(x+1), put(' '), write(i+1), put('\n');
+          write("1 "), write(x+1), put(' '), write(i+3), put('\n');
+          write("1 "), write(x+1), put(' '), write(i+5), put('\n');
+          // std::cout << "1 " << x+1 << ' ' << i+1 << '\n';
+          // std::cout << "1 " << x+1 << ' ' << i+3 << '\n';
+          // std::cout << "1 " << x+1 << ' ' << i+5 << '\n';
         }
-        std::cout << "2 " << r-2 << ' ' << i+1 << '\n';
-        std::cout << "2 " << r-2 << ' ' << i+4 << '\n';
+        write("2 "), write(r-2), put(' '), write(i+1), put('\n');
+        write("2 "), write(r-2), put(' '), write(i+4), put('\n');
+        // std::cout << "2 " << r-2 << ' ' << i+1 << '\n';
+        // std::cout << "2 " << r-2 << ' ' << i+4 << '\n';
       }
       continue;
     }
   }
+  flush();
   return 0;
 }
