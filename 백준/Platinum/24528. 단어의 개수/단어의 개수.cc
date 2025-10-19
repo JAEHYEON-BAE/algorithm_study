95154d3fd7c0384dd373be328e8ef9f651b28395
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-10-19 14:06:39 +0900

    [Platinum IV] Title: 단어의 개수, Time: 44 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Platinum/24528. 단어의 개수/단어의 개수.cc
+++ 백준/Platinum/24528. 단어의 개수/단어의 개수.cc
@@ -4,11 +4,50 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <type_traits>
 
 #define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
 
 using ll=long long int;
 constexpr ll MOD=998'244'353;
+
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+void read(ll &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+void read(std::string &s, int size=0)
+{
+  static char c; s="";
+  if (size) s.reserve(size);
+  for (c=get();c<'!';c=get());
+  for (;c>='!';c=get()) s+=c;
+}
 
 int main() 
 {
@@ -17,17 +56,17 @@ int main()
   std::cout.tie(nullptr);
 
   // dp{i+1}=dp{i} + (dp{i}-dp{prev(c)-1})
-  int n;  std::cin >> n;
+  int n;  read(n);
   ll dp=1;
   std::vector<ll> prev(26, 0LL);
   for (int i=0;i<n;++i) {
-    char c;  ll v;
-    std::cin >> c >> v;
-    ll tmp=v*dp%MOD-(v-1LL)*prev[c-'a']%MOD;
+    std::string c;  ll v;
+    read(c), read(v);
+    ll tmp=v*dp%MOD-(v-1LL)*prev[c[0]-'a']%MOD;
     tmp=(tmp+MOD)%MOD;
-    dp=(v+1LL)*dp%MOD-v*prev[c-'a']%MOD;
+    dp=(v+1LL)*dp%MOD-v*prev[c[0]-'a']%MOD;
     dp=(dp+MOD)%MOD;
-    prev[c-'a']=tmp;
+    prev[c[0]-'a']=tmp;
   }
   std::cout << (dp-1+MOD)%MOD;
   return 0;
