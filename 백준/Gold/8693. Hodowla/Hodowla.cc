c3bfe7be4df6e11f2880d304a0032bbc219ffaa8
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-10-26 17:31:35 +0900

    [Gold IV] Title: Hodowla, Time: 84 ms, Memory: 12012 KB -BaekjoonHub

--- 백준/Gold/8693. Hodowla/Hodowla.cc
+++ 백준/Gold/8693. Hodowla/Hodowla.cc
@@ -7,20 +7,64 @@
 
 #define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
 
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+/* OUTPUT */
+#define OUTPUT_BUFFER_SIZE (1<<20)
+
+char obuf[OUTPUT_BUFFER_SIZE];
+int opos=0;
+inline void flush() 
+{
+  fwrite(obuf, 1, opos, stdout);
+  opos=0;
+}
+inline void put(char c)
+{
+  if (opos==OUTPUT_BUFFER_SIZE) flush();
+  obuf[opos++]=c;
+}
+void write(int x)
+{
+  if (x==0) return put('0');
+  char tmp[11]; int len=0;
+  if (x<0) {put('-');x*=-1;}
+  while (x) {tmp[len++]='0'+(x%10); x/=10;}
+  while (len--) put(tmp[len]);
+}
+
 int main() 
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  int n;  std::cin >> n;
+  int n;  read(n);
   std::vector<std::vector<int>> v(n, std::vector<int>(n, 0));
-  for (int i=0;i<n;++i) for (int j=0;j<n;++j) std::cin >> v[i][j];
+  for (int i=0;i<n;++i) for (int j=0;j<n;++j) read(v[i][j]);
 
-  int m;  std::cin >> m;
+  int m;  read(m);
   std::vector<std::vector<int>> d(n, std::vector<int>(n, 0));
   for (int _=0;_<m;++_) {
-    int c1, r1, c2, r2;  std::cin >> c1 >> r1 >> c2 >> r2;
+    int c1, r1, c2, r2;  read(c1), read(r1), read(c2), read(r2);
     --r1, --c1, --r2, --c2;
     // std::cout << r1 << ' ' << c1 << '\t' << r2 << ' ' << c2 << '\n';
 
@@ -41,10 +85,9 @@ int main()
   }
 
   for (int i=0;i<n;++i) {
-    for (int j=0;j<n;++j) std::cout << (v[i][j]^d[i][j]) << ' ';
-    std::cout << '\n';
+    for (int j=0;j<n;++j) write(v[i][j]^d[i][j]), put(' ');
+    put('\n');
   }
-  
-  
+  flush();
   return 0;
 }
