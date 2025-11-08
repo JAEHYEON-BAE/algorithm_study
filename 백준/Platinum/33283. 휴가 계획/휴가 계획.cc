07cfd87095db6a93e7ac66ebde2d8f81fa0b29a3
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-11-08 13:58:46 +0900

    [Platinum III] Title: 휴가 계획, Time: 176 ms, Memory: 37980 KB -BaekjoonHub

--- 백준/Platinum/33283. 휴가 계획/휴가 계획.cc
+++ 백준/Platinum/33283. 휴가 계획/휴가 계획.cc
@@ -8,6 +8,51 @@
 
 #define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
 
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+
+/* OUTPUT */
+#define OUTPUT_BUFFER_SIZE (1<<20)
+
+char obuf[OUTPUT_BUFFER_SIZE];
+int opos=0;
+inline void flush() 
+{
+  fwrite(obuf, 1, opos, stdout);
+  opos=0;
+}
+inline void put(char c)
+{
+  if (opos==OUTPUT_BUFFER_SIZE) flush();
+  obuf[opos++]=c;
+}
+void write(int x)
+{
+  if (x==0) return put('0');
+  char tmp[11]; int len=0;
+  if (x<0) {put('-');x*=-1;}
+  while (x) {tmp[len++]='0'+(x%10); x/=10;}
+  while (len--) put(tmp[len]);
+}
+
 struct DSU
 {
   int n;
@@ -101,11 +146,11 @@ int main()
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  int n, m;  std::cin >> n >> m;
+  int n, m;  read(n), read(m);
   DSU dsu(n);
   std::vector<std::vector<int>> G(n);
   for (int i=0;i<m;++i) {
-    int u, v;  std::cin >> u >> v;
+    int u, v;  read(u), read(v);
     --u, --v;
     if (dsu.unite(u, v)) {
       G[u].push_back(v);
@@ -115,11 +160,13 @@ int main()
   
   LCA lca(n, G);
   
-  int q;  std::cin >> q;
+  int q;  read(q);
   while (q--) {
-    int s, e;  std::cin >> s >> e;
+    int s, e;  read(s), read(e);
     --s, --e;
-    std::cout << lca.dist(s, e)-1 << '\n';
+    write(lca.dist(s, e)-1);
+    put('\n');
   }
+  flush();
   return 0;
 }
