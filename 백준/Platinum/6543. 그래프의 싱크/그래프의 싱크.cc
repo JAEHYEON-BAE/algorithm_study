a677d21dd51bbca06017c968fa3a76d116ff39b9
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-11-12 08:36:50 +0900

    [Platinum IV] Title: 그래프의 싱크, Time: 8 ms, Memory: 4836 KB -BaekjoonHub

--- 백준/Platinum/6543. 그래프의 싱크/그래프의 싱크.cc
+++ 백준/Platinum/6543. 그래프의 싱크/그래프의 싱크.cc
@@ -7,6 +7,51 @@
 
 #define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
 
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+
+/* OUTPUT */
+#define OUTPUT_BUFFER_SIZE (1<<20)
+
+char obuf[OUTPUT_BUFFER_SIZE];
+int opos=0;
+inline void flush() 
+{
+  fwrite(obuf, 1, opos, stdout);
+  opos=0;
+}
+inline void put(char c)
+{
+  if (opos==OUTPUT_BUFFER_SIZE) flush();
+  obuf[opos++]=c;
+}
+void write(int x)
+{
+  if (x==0) return put('0');
+  char tmp[11]; int len=0;
+  if (x<0) {put('-');x*=-1;}
+  while (x) {tmp[len++]='0'+(x%10); x/=10;}
+  while (len--) put(tmp[len]);
+}
+
 struct SCC
 {
   int n;  
@@ -59,12 +104,13 @@ int main()
   std::cout.tie(nullptr);
 
   int n, m; 
-  while (std::cin >> n) {
+  while (1) {
+    read(n);
     if (!n) break;
-    std::cin >> m;
+    read(m);
     std::vector<std::vector<int>> G(n);
     for (int i=0;i<m;++i) {
-      int v, w;  std::cin >> v >> w;
+      int v, w;  read(v), read(w);
       --v, --w;
       G[v].push_back(w);
     }
@@ -82,9 +128,9 @@ int main()
     }
 
     std::sort(ans.begin(), ans.end());
-    for (const int &u:ans) std::cout << u+1 << ' ';
-    std::cout << '\n';
+    for (const int &u:ans) write(u+1), put(' ');
+    put('\n');
   }
-  
+  flush();
   return 0;
 }
