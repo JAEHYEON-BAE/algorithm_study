2d5e32ceb7498ee47a5fdc885f5b467a4d25274f
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-11-12 11:58:36 +0900

    [Silver III] Title: 트리플 소트, Time: 8 ms, Memory: 3044 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/20309. 트리플 소트/트리플 소트.cc
@@ -0,0 +1,44 @@
+// 20309
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+#define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
+
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+void read(int &x)
+{
+  static char c; x=0;
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+
+int main() 
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr); 
+  std::cout.tie(nullptr);
+
+  int n;  read(n);
+  for (int i=1;i<=n;++i) {
+    int x;  read(x);
+    if (std::abs(x-i)&1) {std::cout << "NO"; return 0;}
+  }
+  std::cout << "YES";
+  return 0;
+}
