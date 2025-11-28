486258fb1ecfbc76d65d0f01833ee29afc84a7d3
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-11-28 12:33:04 +0900

    [Platinum III] Title: Reversi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/30430. Reversi/Reversi.cc
+++ 백준/Platinum/30430. Reversi/Reversi.cc
@@ -1,3 +1,3 @@
 #include<iostream>
 using namespace std;
-int main(){cin.tie(0);int t,n,m,x;cin>>t;while(t--){cin>>n>>m;x=n*m%4;cout<<((!x)?'D':(x==1)?'W':'L');}}
+int main(){cin.tie(0);int t,n,m;cin>>t;while(t--){cin>>n>>m;cout<<"DWLL"[n*m%4];}}
