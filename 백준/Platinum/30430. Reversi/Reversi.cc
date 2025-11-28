8cc13194847ad980dd1665ebc5192942fcce5528
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-11-28 12:31:27 +0900

    [Platinum III] Title: Reversi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/30430. Reversi/Reversi.cc
+++ 백준/Platinum/30430. Reversi/Reversi.cc
@@ -1,23 +1,3 @@
-// 30430
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-#define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
-
-int main() 
-{
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr); 
-  std::cout.tie(nullptr);
-
-  int t;  std::cin >> t;
-  while (t--) {
-    int n, m;  std::cin >> n >> m;
-    int x=n*m%4;
-    std::cout << ((x==0)?'D':(x==1)?'W':'L');
-  }
-  return 0;
-}
+#include<iostream>
+using namespace std;
+int main(){cin.tie(0);int t,n,m,x;cin>>t;while(t--){cin>>n>>m;x=n*m%4;cout<<((!x)?'D':(x==1)?'W':'L');}}
