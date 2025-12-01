3247d1d9400f71708565cdc9e59e817af05092e4
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-12-01 12:36:29 +0900

    [Platinum II] Title: 하늘에서 떨어지는 1, 2, ..., R-L+1개의 별, Time: 20 ms, Memory: 6424 KB -BaekjoonHub

--- 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
+++ 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
@@ -9,6 +9,53 @@
 
 using ll=long long int;
 
+/* INPUT */
+#define INPUT_BUFFER_SIZE (1<<20)
+char get()
+{
+  static char buf[INPUT_BUFFER_SIZE], *S=buf, *T=buf;
+  if (S==T) {
+    T=(S=buf)+fread(buf, 1, INPUT_BUFFER_SIZE, stdin);
+    if (S==T) return EOF;
+  }
+  return *S++;
+}
+template <typename T>
+void read(T &x)
+{
+  static char c; x=T(0);
+  bool MINUS_FLAG=0;
+  for (c=get();(c<'0'||c>'9')&&c!='-';c=get());
+  if (c=='-') {MINUS_FLAG=1;c=get();}
+  for (;c>='0'&&c<='9';c=get()) x=x*10+(c-'0');
+  if (MINUS_FLAG) x*=-1;
+}
+
+/* OUTPUT */
+#define OUTPUT_BUFFER_SIZE (1<<20)
+
+char obuf[OUTPUT_BUFFER_SIZE];
+int opos=0;
+inline void flush() 
+{
+  fwrite(obuf, 1, opos, stdout);
+  opos=0;
+}
+inline void put(char c)
+{
+  if (opos==OUTPUT_BUFFER_SIZE) flush();
+  obuf[opos++]=c;
+}
+template <typename T>
+void write(T x)
+{
+  if (x==0) return put('0');
+  char tmp[11]; int len=0;
+  if (x<0) {put('-');x*=-1;}
+  while (x) {tmp[len++]='0'+(x%10); x/=10;}
+  while (len--) put(tmp[len]);
+}
+
 struct Fenwick
 {
   // 1 ~ n
@@ -56,28 +103,29 @@ int main()
   std::cin.tie(nullptr); 
   std::cout.tie(nullptr);
 
-  int n;  std::cin >> n;
+  int n;  read(n);
   std::vector<ll> a(n);
   RURQ rurq(n);
-  for (ll &x:a) std::cin >> x;
+  for (ll &x:a) read(x);
 
   // DIFFERENTIAL ARRAY !
 
-  int q;  std::cin >> q;
+  int q;  read(q);
   while (q--) {
-    int cmd;  std::cin >> cmd;
+    int cmd;  read(cmd);
     if (cmd==1) {
-      int l, r;  std::cin >> l >> r;
+      int l, r;  read(l), read(r);
       rurq.update(l, r, 1);
       if (r+1<=n) {
         rurq.update(r+1, r+1, -1LL*(r+1-l));
       }
     }
     else {
-      int x;  std::cin >> x;
-      std::cout << rurq.query(x)+a[x-1] << '\n';
+      int x;  read(x);
+      write(rurq.query(x)+a[x-1]);
+      put('\n');
     }
   }
-  
+  flush();
   return 0;
 }
