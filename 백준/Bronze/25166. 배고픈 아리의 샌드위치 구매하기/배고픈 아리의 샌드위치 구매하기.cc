885ba70f3549a6197e044bf265f38ca2e4024ce3
Jaehyeon <154305251+JAEHYEON-BAE@users.noreply.github.com>
2025-12-03 20:03:09 +0900

    [Bronze I] Title: 배고픈 아리의 샌드위치 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.cc
+++ 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.cc
@@ -1,22 +1,2 @@
-// 25166
-
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
-int main()
-{
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
-  
-  int s, m;  std::cin >> s >> m;
-  if (s<1024) std::cout << "No thanks";
-  else {
-    s-=1023;
-    if ((m&s)==s) std::cout << "Thanks";
-    else std::cout << "Impossible";
-  }
-  
-  return 0;
-}
+int main(){int s,m;std::cin>>s>>m;std::cout<<(s<1024?"No thanks":((m|(s-1023))==m)?"Thanks":"Impossible");}
